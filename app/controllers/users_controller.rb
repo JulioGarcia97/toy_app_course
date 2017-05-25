17163bf896d1feed5f4ccf29ad390a6cde28e4a8
Julio Garcia <cggjulio@gmail.com>
2017-05-25 16:42:05 -0500

    Fix micropost error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,6 @@ class UsersController < ApplicationController
   # GET /users.json
   def index
     @users = User.all
-    @user = User.first
   end
 
   # GET /users/1
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-	has_many :microposts
+	has_many :micropost
 
 	validates :name, :email, presence: true
 end
